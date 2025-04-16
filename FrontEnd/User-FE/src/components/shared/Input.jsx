e4d662c931bb4e237d5b4305dcbd0821ac6102e0
phanduc0136@gmail.com <phanduc0136@gmail.com>
2025-04-16 21:32:14 +0700

    make reuse component

---
+++ FrontEnd/User-FE/src/components/shared/Input.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable react/prop-types */
+
+const Input = ({ type, placeholder }) => {
+  return (
+    <input
+      type={type}
+      placeholder={placeholder}
+      className="w-full p-3 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+    />
+  );
+};
+
+export default Input;
--- FrontEnd/User-FE/src/pages/Login.jsx
+++ FrontEnd/User-FE/src/pages/Login.jsx
@@ -1,6 +1,7 @@
 import React from "react"; // eslint-disable-line no-unused-vars
 import backgroundImage from "../assets/page-signup-signin/sign-in.jpg";
 import { FaFacebook, FaGoogle } from "react-icons/fa";
+import Input from "../components/shared/Input";
 
 const LoginPage = () => {
   return (
@@ -27,16 +28,8 @@ const LoginPage = () => {
           </div>
           <h2 className="text-2xl font-bold text-center mb-6">Sign In</h2>
           <form className="space-y-4 w-full max-w-sm">
-            <input
-              type="email"
-              placeholder="Email"
-              className="w-full p-3 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-            />
-            <input
-              type="password"
-              placeholder="Password"
-              className="w-full p-3 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-            />
+            <Input type="email" placeholder="Email"/>
+            <Input type="password" placeholder="Password"/>
             <button
               type="submit"
               className="w-full bg-blue-600 text-white p-3 rounded-md hover:bg-blue-700 transition duration-300"
