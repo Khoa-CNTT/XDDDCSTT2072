7fde7c9eec84025a9264fc99a20865c5db9256b9
DuyTruyen1 <truyenmap420@gmail.com>
2025-05-03 10:54:52 +0700

    fix marketplace

--- Backend/Agricultural/build/generated/sources/annotationProcessor/java/main/com/agricultural/agricultural/mapper/OrderMapperImpl.java
+++ Backend/Agricultural/build/generated/sources/annotationProcessor/java/main/com/agricultural/agricultural/mapper/OrderMapperImpl.java
@@ -13,7 +13,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-04-29T14:08:48+0700",
+    date = "2025-05-03T10:53:19+0700",
     comments = "version: 1.5.5.Final, compiler: IncrementalProcessingEnvironment from gradle-language-java-8.12.1.jar, environment: Java 23.0.2 (Oracle Corporation)"
 )
 @Component
